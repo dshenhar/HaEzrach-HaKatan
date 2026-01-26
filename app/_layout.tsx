4ccabb290a598fde45984be9d280b596d78d0380
shenhars <shenhars03@gmail.com>
2026-01-26 13:13:02 +0200

    fix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,8 @@
 import { Stack } from 'expo-router';
+import { I18nManager } from 'react-native';
+
+I18nManager.allowRTL(true);
+I18nManager.forceRTL(true);
 
 export default function RootLayout() {
 	return (
